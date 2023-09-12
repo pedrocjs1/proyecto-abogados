a663d19282b64774e2e4c505420bedd8ed6366d1
Pedro Vega <pedrovega468@yahoo.com>
2023-09-12 17:00:34 -0300

    nav correct practice

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,9 @@ import LogoWhite from '../images/logo-abogados-white.png'
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1200);
+  const [currentLogo, setCurrentLogo] = useState(isWideScreen ? LogoWhite : Logo);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -19,72 +22,65 @@ function Navbar() {
     setMenuOpen(false);
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const linksWhiteRules = document.querySelectorAll('.links-white-rule');
-      const logoElement = document.querySelector('.site-logo img');
+  const updateLogo = () => {
+    if (isWideScreen) {
+      setCurrentLogo(isScrolled ? Logo : LogoWhite);
+    } else {
+      setCurrentLogo(Logo);
+    }
+  };
 
-        if (window.scrollY > 30) { 
-            document.querySelector('.navbar-area').classList.add('navbar-scrolled');
-            document.querySelector('.a-white-rule').classList.add('a-white-rule-xxx');
-            document.querySelector('.span-white-rule').classList.add('span-white-rule-xxx');
-            linksWhiteRules.forEach(el => el.classList.add('links-white-rule-xxx'));
-            
-            if (window.innerWidth > 1200) {
-              logoElement.src = Logo; 
-            }
-        } else {
-            document.querySelector('.navbar-area').classList.remove('navbar-scrolled');
-            document.querySelector('.a-white-rule').classList.remove('a-white-rule-xxx');
-            document.querySelector('.span-white-rule').classList.remove('span-white-rule-xxx');
-            linksWhiteRules.forEach(el => el.classList.remove('links-white-rule-xxx'));
-            
-            if (window.innerWidth > 1200) {
-              logoElement.src = LogoWhite; 
-            }
-            if (window.innerWidth < 1200) {
-            logoElement.src = Logo; 
-            }
-        }
+  useEffect(() => {
+    const handleResize = () => {
+      const wideScreen = window.innerWidth > 1200;
+      setIsWideScreen(wideScreen);
+      updateLogo();
     };
+
+    const handleScroll = () => {
+      if (window.scrollY > 30) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+      updateLogo();
+    };
+
+    window.addEventListener('resize', handleResize);
     window.addEventListener('scroll', handleScroll);
     return () => {
-        window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
     };
-}, []);
+  }, [isWideScreen, isScrolled]);
 
 
   return (
-    <div className="navbar-area navbar-custom">
+    <div className={`navbar-area navbar-custom ${isScrolled ? 'navbar-scrolled' : ''}`}>
       <div className="container">
-        <nav className="site-navbar ">
+        <nav className="site-navbar">
           <a href="#inicio" className="site-logo">
-            <img src={Logo} alt="Logo" className="" />
+            <img src={currentLogo} alt="Logo" />
           </a>
 
           <div className='w-100 nav-container-ul-number'>
             <div className='w-100 d-flex flex-column align-items-end me-5 container-nav-container-ul-number'>
-              <span className='pe-3 span-white-rule free-span'>24/7 FREE CONSULTATION</span>
-              <a href="" className='pe-3 a-white-rule number-span'>702-333-1111</a>
+              <span className={`pe-3 ${isWideScreen && !isScrolled ? 'span-white-rule' : 'span-white-rule-xxx'} free-span`}>24/7 FREE CONSULTATION</span>
+              <a href="" className={`pe-3 ${isWideScreen && !isScrolled ? 'a-white-rule' : 'a-white-rule-xxx'} number-span`}>702-333-1111</a>
             </div>
             <div className='w-100 d-flex align-items-center justify-content-end containter-ul-nav'>
-
               <ul className={menuOpen ? 'open' : ''} onClick={closeMenu}>
-                <li><a className='links-white-rule' href="#inicio">Inicio</a></li>
-                <li><a className='links-white-rule' href="#about">Sobre Mi</a></li>
-                <li><a className='links-white-rule' href="#services">Servicios</a></li>
-                <li><a className='links-white-rule' href="#portfolio">Portafolio</a></li>
-                <li><a className='links-white-rule' href="#contact">Contacto</a></li>
+                <li><a className={`${isWideScreen && !isScrolled ? 'links-white-rule' : 'links-white-rule-xxx'}`} href="#inicio">Inicio</a></li>
+                <li><a className={`${isWideScreen && !isScrolled ? 'links-white-rule' : 'links-white-rule-xxx'}`} href="#about">Sobre Mi</a></li>
+                <li><a className={`${isWideScreen && !isScrolled ? 'links-white-rule' : 'links-white-rule-xxx'}`} href="#services">Servicios</a></li>
+                <li><a className={`${isWideScreen && !isScrolled ? 'links-white-rule' : 'links-white-rule-xxx'}`} href="#portfolio">Portafolio</a></li>
+                <li><a className={`${isWideScreen && !isScrolled ? 'links-white-rule' : 'links-white-rule-xxx'}`} href="#contact">Contacto</a></li>
               </ul>
               <button className={`nav-toggler ${menuOpen ? 'toggler-open' : ''}`} onClick={toggleMenu}>
-            <span></span>
-          </button>
+                <span></span>
+              </button>
             </div>
           </div>
-          
-
-          {/* nav-toggler for mobile version only */}
-          
         </nav>
       </div>
     </div>
