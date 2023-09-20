d28fd331f900d2e855afed5fc42c90e9707df76e
Pedro Vega <pedrovega468@yahoo.com>
2023-09-20 19:21:52 -0300

    grandes cambios

--- server/routes.js
+++
@@ -1,7 +0,0 @@
-const { Wit } = require("node-wit");
-const client = new Wit({ accessToken: "TU_ACCESS_TOKEN" });
-
-app.post("/chat", async (req, res) => {
-  const userMessage = req.body.message;
-  const witResponse = await client.message(userMessage, {});
-});
